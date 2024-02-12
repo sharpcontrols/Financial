7057f4af0727e96be5cc13c2d54e94524278e68e
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-02-12 14:17:55 -0300

    Added function to return overdue profits/expenses
    As described in task 1 and 2 in #2

---
+++ Utils.cs
@@ -0,0 +1,34 @@
+﻿using SharpControls.Financial.Models.Financial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpControls.Financial
+{
+    public static class Utils
+    {
+        public static List<Profit> OverdueProfitsWithinDays(Database database, int days)
+        {
+            return OverdueProfitsWithinDays(database.Profits, days);
+        }
+
+        public static List<Profit> OverdueProfitsWithinDays(List<Profit> profits, int days)
+        {
+            DateTime dueDate = DateTime.Now.AddDays(days);
+            return profits.Where(p => p.DueDate >= DateTime.Now && p.DueDate <= dueDate).ToList();
+        }
+
+        public static List<Expense> OverdueExpensesWithinDays(Database database, int days)
+        {
+            return OverdueExpensesWIthinDays(database.Expenses, days);
+        }
+
+        public static List<Expense> OverdueExpensesWIthinDays(List<Expense> expenses, int days)
+        {
+            DateTime dueDate = DateTime.Now.AddDays(days);
+            return expenses.Where(e => e.DueDate >= DateTime.Now && e.DueDate <= dueDate).ToList();
+        }
+    }
+}
